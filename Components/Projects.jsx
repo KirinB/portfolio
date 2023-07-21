ad3cb82ec1cb13fdf4ac3cade73a33bd71896a41
KirinB <lee.minhnhan29@gmail.com>
2023-07-21 10:24:40 +0700

    basement

---
+++ Components/Projects.jsx
@@ -0,0 +1,44 @@
+import ProjectItem from "./ProjectItem";
+import ethanImg from "../public/assets/projects/ethan.jpg";
+import travelImg from "../public/assets/projects/travel.jpg";
+import netflixImg from "../public/assets/projects/netflix.jpg";
+import youtubeImg from "../public/assets/projects/youtube.jpg";
+function Projects() {
+  return (
+    <div id="projects" className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+          Projects
+        </p>
+        <h2 className="py-4">What I've Built</h2>
+        <div className="grid md:grid-cols-2 gap-8">
+          <ProjectItem
+            title="Ethan Portfolio Clone"
+            backgroundImg={ethanImg}
+            projectUrl="/ethan"
+            library="NextJS"
+          />
+          <ProjectItem
+            title="TravelPu"
+            backgroundImg={travelImg}
+            projectUrl="/travel"
+            library={"React"}
+          />
+          <ProjectItem
+            title="Netflix UI"
+            backgroundImg={netflixImg}
+            projectUrl="/netflix"
+          />
+          <ProjectItem
+            title="Youtube UI"
+            backgroundImg={youtubeImg}
+            projectUrl="/youtube"
+            library={"React"}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
---
+++ pages/netflix.jsx
@@ -0,0 +1,80 @@
+import Image from "next/image";
+import React from "react";
+import netflixImg from "../public/assets/projects/netflix.jpg";
+import { RiRadioButtonFill } from "react-icons/ri";
+import Link from "next/link";
+import { BiArrowBack } from "react-icons/bi";
+
+const property = () => {
+  return (
+    <div className="w-full">
+      <div className="w-screen h-[50vh] relative">
+        <div className="absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10" />
+        <Image
+          className="absolute z-1"
+          layout="fill"
+          objectFit="cover"
+          src={netflixImg}
+          alt="/"
+        />
+        <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
+          <h2 className="py-2">Netflix Ui</h2>
+          <h3>React / Tailwind</h3>
+        </div>
+      </div>
+
+      <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8">
+        <div className="col-span-4">
+          <p>Project</p>
+          <h2 className="py-4">Overview</h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+          <div className="flex justify-center md:justify-start my-4">
+            <a
+              href="https://github.com/KirinB/ethan-portfolio"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <button className="px-8 py-2 mt-4 mr-8">Code</button>
+            </a>
+            <a href="/" target="_blank" rel="noreferrer">
+              <button className="px-8 py-2 mt-4">Demo</button>
+            </a>
+          </div>
+        </div>
+        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4">
+          <div className="p-4">
+            <p className="text-center font-bold pb-2">Technologies</p>
+            <div className="grid grid-cols-3 md:grid-cols-1">
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> React
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Tailwind
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Javascript
+              </p>
+
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Framer Motion
+              </p>
+            </div>
+          </div>
+        </div>
+        <Link href="/#projects">
+          <div className="underline cursor-pointer flex items-center ml-2 md:ml-0">
+            <BiArrowBack size={18} />
+            <p className="ml-4">Back</p>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default property;
